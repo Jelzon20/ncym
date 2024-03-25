c8754ad5b6545c99f5711eb98ea186de9bd22987
jelzon20 <ja.holasca2@gmail.com>
2024-03-25 23:16:56 +0800

    create header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,18 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
+import Primer from './pages/Primer'
+import Header from './components/Header'
+import Program from './pages/Program'
+import Workshops from './pages/Workshops'
+import Evaluations from './pages/Evaluations'
+import Host from './pages/Host'
+import ContactUs from './pages/ContactUs'
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/about' element={<About />}/>
@@ -17,6 +25,12 @@ export default function App() {
         <Route path='/sign-up' element={<SignUp />}/>
         <Route path='/dashboard' element={<Dashboard />}/>
         <Route path='/projects' element={<Projects />}/>
+        <Route path='/primer' element={<Primer />}/>
+        <Route path='/program' element={<Program />}/>
+        <Route path='/workshops' element={<Workshops />}/>
+        <Route path='/evaluation' element={<Evaluations />}/>
+        <Route path='/host' element={<Host />}/>
+        <Route path='/contact-us' element={<ContactUs />}/>
       </Routes>
    
    
