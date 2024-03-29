4db6845ff7a68aa53014eaf829b392858c9e94ec
jelzon20 <ja.holasca2@gmail.com>
2024-03-29 14:02:44 +0800

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,12 +1,18 @@
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from '../redux/user/userSlice';
 // import OAuth from '../components/OAuth';
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const { loading, error: errorMessage } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
@@ -14,12 +20,10 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!formData.email || !formData.password) {
-      return setErrorMessage('Please fill out all fields.');
-    } 
-    
+      return dispatch(signInFailure('Please fill all the fields'));
+    }
     try {
-      setLoading(true);
-      setErrorMessage(null);
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -27,22 +31,17 @@ export default function SignIn() {
       });
       const data = await res.json();
       if (data.success === false) {
-        setErrorMessage(data.message);
-        setLoading(false);
+        dispatch(signInFailure(data.message));
       }
-      setLoading(false);
-      if(res.ok) {
+
+      if (res.ok) {
+        dispatch(signInSuccess(data));
         navigate('/');
       }
-    } 
-    catch (error) {
-      setErrorMessage(error.message);
-      setLoading(false);
+    } catch (error) {
+      dispatch(signInFailure(error.message));
     }
-    
   };
-
-  console.log(loading);
   return (
     <div className='min-h-screen mt-20'>
       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
@@ -50,12 +49,12 @@ export default function SignIn() {
         <div className='flex-1'>
           <Link to='/' className='font-bold dark:text-white text-4xl'>
             <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
-              NCYM
+              Sahand's
             </span>
-            2024
+            Blog
           </Link>
           <p className='text-sm mt-5'>
-            This is a demo project. You can sign up with your email and password
+            This is a demo project. You can sign in with your email and password
             or with Google.
           </p>
         </div>
@@ -64,7 +63,7 @@ export default function SignIn() {
         <div className='flex-1'>
           <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
             <div>
-              <Label value='Email' className='font-semibold' />
+              <Label value='Your email' />
               <TextInput
                 type='email'
                 placeholder='name@company.com'
@@ -73,10 +72,10 @@ export default function SignIn() {
               />
             </div>
             <div>
-              <Label value='Password' className='font-semibold' />
+              <Label value='Your password' />
               <TextInput
                 type='password'
-                placeholder='********'
+                placeholder='**********'
                 id='password'
                 onChange={handleChange}
               />
@@ -95,10 +94,10 @@ export default function SignIn() {
                 'Sign In'
               )}
             </Button>
-            {/* <OAuth /> */}
+            {/* <OAuth />  */}
           </form>
           <div className='flex gap-2 text-sm mt-5'>
-            <span>Don't have an account?</span>
+            <span>Dont Have an account?</span>
             <Link to='/sign-up' className='text-blue-500'>
               Sign Up
             </Link>
